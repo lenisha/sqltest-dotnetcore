b54eca70521491a7cbd22997ae2796df952cb99d
Elena Neroslavskaya <lenisha@gmail.com>
2019-10-02 15:24:25 -0400

    Add sql operator

--- Controllers/EditorController.cs
+++ Controllers/EditorController.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 using System.Data.SqlClient;
 using System.Web;
+using System.IO;
 
 namespace CloudQuery.Controllers
 {
     public class EditorController : Controller
     {
+        private const string AzureSecretsMountPath = "/etc/sqlservice";
+
         public IActionResult Index()
         {
             // let's set ViewData value for Connections
@@ -25,9 +28,12 @@ namespace CloudQuery.Controllers
             qr.Message = "Query execution incomplete";
 
             // get the connectionObject
-            var conn = Newtonsoft.Json.JsonConvert.DeserializeObject<CloudQuery.Models.DBConnection>(connections);            
-            string connectionString = string.Format("Server=tcp:{0},1433;Database={1};Uid={2}@{0};Pwd={3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;", conn.DBServer, conn.DBName, conn.Username, conn.Password);
+            var conn = Newtonsoft.Json.JsonConvert.DeserializeObject<CloudQuery.Models.DBConnection>(connections); 
+            if ( conn.DBName.StartsWith("#") )
+                PopulateConnectionDataFromMount(conn);          
+            string connectionString = string.Format("Server=tcp:{0}.database.windows.net,1433;Database={1};Uid={2}@{0};Pwd={3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;", conn.DBServer, conn.DBName, conn.Username, conn.Password);
             connectionString = "Driver={ODBC Driver 17 for SQL Server};" + connectionString;
+            Console.WriteLine($"ConnectionString: {connectionString}");
 
             try
             {
@@ -63,5 +69,20 @@ namespace CloudQuery.Controllers
             ViewData["results"] = qr;
             return View("Index");
         }
+
+        void  PopulateConnectionDataFromMount(CloudQuery.Models.DBConnection conn) 
+        {
+            Console.WriteLine($"Processing secrets: {AzureSecretsMountPath}");
+
+            string data = System.IO.File.ReadAllText(AzureSecretsMountPath +"/username");
+            Console.WriteLine($"Secret 'username' has value '{data}'");
+            conn.Username = data;
+            data = System.IO.File.ReadAllText(AzureSecretsMountPath +"/password");
+            conn.Password = data;
+            data = System.IO.File.ReadAllText(AzureSecretsMountPath +"/sqlservername");
+            conn.DBServer = data;
+            conn.DBName = conn.DBName.Substring(1);
+        }
     }
+
 }
